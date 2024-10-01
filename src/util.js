aba370c62e6a7e9f91d4c0433d1052a8b62b705c
peterdanwan <peterdanwan@gmail.com>
2024-10-01 18:22:41 -0400

    feat(util.js): add getConfig() function which returns object with variables in .barrierless

--- src/util.js
+++ src/util.js
@@ -1,4 +1,26 @@
 import path from 'path';
+import toml from 'toml';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+// Retrieve the values within the TOML config file (.barrierless), and export them as the `config` object
+export function getConfig() {
+  // Logic to read the values from the .barrierless config file in the home directory
+  const os = require('os');
+  const fs = require('fs');
+  const __homedir = os.homedir();
+  const tomlFilePath = path.join(__homedir, '.barrierless');
+
+  let config = {};
+  try {
+    const configFileContent = fs.readFileSync(tomlFilePath, 'utf-8');
+    config = toml.parse(configFileContent);
+  } catch (error) {
+    console.error(`Error reading TOML file: ${error.message}`);
+  }
+
+  return config;
+}
 
 // Check file extension to ensure it is a .txt file
 export function txtFilenameExt(filename) {
